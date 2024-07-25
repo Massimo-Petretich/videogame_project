49ff865e1b8d061488eae04c8d8121efce494d52
Massimo Petretich <67137988+Massimo-Petretich@users.noreply.github.com>
2024-07-25 07:37:27 +0200

    Delete extract_canvas.js

--- extract_canvas.js
+++
@@ -1,7 +0,0 @@
-function scaleCanvasElement() {
-	const canvas = document.getElementById("canvas2D");
-	canvas.width = window.innerWidth;
-	canvas.height = window.innerHeight;
-}
-scaleCanvasElement();
-window.addEventListener("resize", scaleCanvasElement);
